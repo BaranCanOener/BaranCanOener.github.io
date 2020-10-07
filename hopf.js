786c0a310b056412f286e85a46f87495892548ae
Baran Can Öner <54043669+BaranCanOener@users.noreply.github.com>
2020-10-07 16:13:15 +0100

    Add files via upload

--- hopf.js
+++ hopf.js
@@ -271,6 +271,17 @@ function initGui() {
     } );
     globalOptions.add( param, 'Map R3 to B3' ).onChange( function ( val ) {
         compressToBall = val;
+
+        var index = baseSpaceCircles.length-1;
+        var pointCount = baseSpaceCircles[index].pointCount;
+        var defaultRotation = baseSpaceCircles[index].defaultRotation;
+        var distanceToCenter = baseSpaceCircles[index].distanceToCenter;
+        var circumference = baseSpaceCircles[index].circumference;
+        var appliedRotation_axis = baseSpaceCircles[index].appliedRotation_axis;
+        var appliedRotation_angle = baseSpaceCircles[index].appliedRotation_angle;
+        baseSpaceCircles.pop().destroy();
+        baseSpaceCircles.push(new baseSpaceCircle(distanceToCenter, circumference, pointCount, defaultRotation, appliedRotation_axis.normalize(), appliedRotation_angle));
+        
         for (var index in baseSpaceCircles)
             baseSpaceCircles[index].updateFiberProjections();
         render();
